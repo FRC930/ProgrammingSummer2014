30f58ed4b4dc6d6db19159d12d48a0b70c3d93df
Greg Billetdeaux <gregadeaux@gmail.com>
2014-06-19 12:48:09 -0500

    Added java project

---
+++ Hello930/src/hello930/Hello930.java
@@ -0,0 +1,13 @@
+package hello930;
+
+/**
+ *
+ * @author gregbilletdeaux
+ */
+public class Hello930 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello 930");
+    }
+    
+}
