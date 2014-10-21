5c7ea136d0b040214e70c44e10ecf5e13b2a8ee2
Owen Zinkgraf <ozinkgraf@gmail.com>
2014-10-20 19:28:31 -0500

    I changed 930 to Owen

--- Hello930/src/hello930/Hello930.java
+++ Hello930/src/hello930/Hello930.java
@@ -7,7 +7,7 @@ package hello930;
 public class Hello930 {
 
     public static void main(String[] args) {
-        System.out.println("Hello FRC930");
+        System.out.println("Hello Owen Zinkgraf");
         System.out.println("GOODBYE 930");
     }
     
