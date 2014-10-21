5d36dd7d8cfe56daf48f1e05a23e2fd749f4fc13
Brian Scharles <bpscharles@gmail.com>
2014-10-20 19:28:16 -0500

    added my name

--- Hello930/src/hello930/Hello930.java
+++ Hello930/src/hello930/Hello930.java
@@ -7,7 +7,7 @@ package hello930;
 public class Hello930 {
 
     public static void main(String[] args) {
-        System.out.println("Hello FRC930");
+        System.out.println("Hello Becky");
         System.out.println("GOODBYE 930");
     }
     
